89560755b6adb4a9138c81a2f8747b23101913aa
KarthikaRajagopal44 <karthiizz.444@gmail.com>
2024-10-01 22:09:52 +0530

    initial commit

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.index, name='list'),
     path('update_task/<str:pk>/', views.updateTask, name="update_task"),
+    path('delete_task/<str:pk>/', views.deleteTask, name="delete_task"),
 ]
